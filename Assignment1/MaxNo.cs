2e101753fe930ab128b5dd122c4ca2ec216ef525
SnehalS26 <Snehalshirsath26@gmail.com>
2023-06-09 14:44:05 +0530

    Add new Program

---
+++ Assignment1/MaxNo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Write a program to find maximum between three numbers 
+namespace Assignment1
+{
+    internal class MaxNo
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+
+            Console.WriteLine("Enter 1st Number:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter 2nd Number:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter 3rd Number:");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.WriteLine("Number 1st is Larger.");
+                }
+                else
+                {
+                    Console.WriteLine("Number 3rd is Larger.");
+                }
+            }
+            else if(num2 > num3)
+            {
+                Console.WriteLine("Number 2nd is Larger.");
+            }
+            else
+            {
+                Console.WriteLine("Number 3rd is Larger.");
+
+            }
+        }
+    }
+}
---
+++ Assignment1/vowel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+//Write a program to input any alphabet and check whether it is vowel or consonant 
+namespace Assignment1
+{
+    internal class vowel
+    {
+        static void Main(string[] args)
+        {
+            char c;
+            Console.WriteLine("Enter a Alphabets:");
+            c = Convert.ToChar(Console.ReadLine());
+            
+            if(c == 'a' || c== 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'U' || c == 'O')
+            {
+                Console.WriteLine($"{c} is a Vowel.");
+            }
+            else if((c >= 'a' &&  c <= 'z') || (c >= 'A' && c <= 'Z'))
+                    {
+                Console.WriteLine($"{c} is a Consonant.");
+                    }
+            else
+            {
+                Console.WriteLine(" ");
+            }
+
+
+        }
+    }
+}
